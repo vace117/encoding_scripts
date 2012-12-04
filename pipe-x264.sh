f3f0ecedbeb3026d61ae046470388b30bda8d29a
val <blantv@agr.gc.ca>
2012-12-04 16:15:27 -0600

    pipe-x264

--- pipe-x264.sh
+++ pipe-x264.sh
@@ -1,34 +1,28 @@
 #!/bin/sh
 #
-def_interlace=1
-def_fast=2
-def_pass=1
+# Use this script to export Sony A57 videos from Cinelerra
+#
+pass=1
+bit_rate=10000
 
 help(){
     cat <<END
 Usage: $0 [options] filename.anything
 Options:
-    -i n      n=0 prog, n=1 top, n=2 bot            ($def_interlace)
-    -q n      n=1 slow, n=2 fast                    ($def_fast)
-    -p n      pass n                                ($def_pass)
+    -b n      bitrate n                             ($bit_rate)
+    -p n      pass n                                ($pass)
     -h        Print this help message
 END
     exit 0
 }
 
-interlace=$def_interlace
-fast=$def_fast
-pass=$def_pass
-
-while getopts i:p:q:h name "$@"
+while getopts b:p:h name "$@"
 do
     case $name in
-i)
-    interlace=$OPTARG ;;
+b)
+	bit_rate=$OPTARG ;;
 p)
 	pass=$OPTARG ;;
-q)
-    subq=$OPTARG ;;
 *)
     help ;;
     esac
@@ -43,72 +37,23 @@ fi
 outfile=$1
 base=`echo $outfile | sed "s/\.[^.]*$//"`
 
-case $interlace in
-0)
-    iflag_m=NOT_INTERLACED
-	xint_g="--direct auto" ;;
-1)
-    iflag_m=INTERLACED_TOP_FIRST
-	xint_g="--direct spatial --interlaced --tff" ;;
-*)
-	iflag_m=INTERLACED_BOTTOM_FIRST
-	xint_g="--direct spatial --interlaced --bff" ;;
-esac
+command="x264 /tmp/cine_pipe --input-res 1920x1080 --fps 59.9401 --bitrate $bit_rate \
+    --pass $pass --stats \"$base.stats\" \
+    --bframes 2 --b-adapt 2 \
+    --threads auto \
+    --output \"$outfile\""
 
-case $rescale in
-1)
-	xint_f="nointerlaced" ;;
-*)
-	scale_f="" ;;
-esac
+# Make a named pipe
+rm /tmp/cine_pipe 2> /dev/null
+mkfifo /tmp/cine_pipe
 
-echo "Encoding yuv4mpeg on /dev/stdin to $base.264"
+echo "Running pass $pass:"
+echo "     $command"
+echo
 
-case $fast in
-1)
-echo x264 --fps 30000/1001 --sar 4:3 --pass $pass --bitrate 10000 \
-    --stats $base.stats --level 4.1 --keyint 14 --min-keyint 2 \
-    --ref 3 --mixed-refs --bframes 2 --b-adapt 2 --weightb $xint_g \
-    --deblock -1:-1 --subme 7 \
-    --trellis 2 --partitions p8x8,b8x8,i4x4,i8x8 --8x8dct \
-    --ipratio 1.1 --pbratio 1.1 \
-    --vbv-bufsize 14475 --vbv-maxrate 17500 --qcomp 0.5 \
-    --me umh --threads auto \
-    --output $outfile /dev/stdin 1440x1080 \
-    --mvrange 511 --aud
+# Run the encoding command. It will block and wait for y4mtoyuv to start feeding data into the pipe
+eval "$command &"
 
-y4mtoyuv |
-x264 --fps 30000/1001 --sar 4:3 --pass $pass --bitrate 10000 \
-    --stats $base.stats --level 4.1 --keyint 14 --min-keyint 2 \
-    --ref 3 --mixed-refs --bframes 2 --b-adapt 2 --weightb $xint_g \
-    --deblock -1:-1 --subme 7 \
-    --trellis 2 --partitions p8x8,b8x8,i4x4,i8x8 --8x8dct \
-    --ipratio 1.1 --pbratio 1.1 \
-    --vbv-bufsize 14475 --vbv-maxrate 17500 --qcomp 0.5 \
-    --me umh --threads auto \
-    --output $outfile /dev/stdin 1440x1080 \
-    --mvrange 511 --aud ;;
-*)
-echo x264 --fps 30000/1001 --sar 4:3 --pass $pass --bitrate 10000 \
-	--stats $base.stats --level 4.1 --keyint 14 --min-keyint 2 \
-	--ref 2 --mixed-refs --bframes 2 --weightb $xint_g \
-	--deblock -1:-1 --subme 5 \
-	--partitions p8x8,b8x8,i4x4,i8x8 --8x8dct \
-	--ipratio 1.1 --pbratio 1.1 \
-	--vbv-bufsize 14475 --vbv-maxrate 17500 --qcomp 0.5 \
-	--merange 12 --threads auto \
-	--output $outfile /dev/stdin 1440x1080 \
-	--mvrange 511 --aud
+# Pipe the input from Cinelerra into y4mtoyuv, which pipes its output into the named pipe
+cat | y4mtoyuv > /tmp/cine_pipe
 
-y4mtoyuv |
-x264 --fps 30000/1001 --sar 4:3 --pass $pass --bitrate 10000 \
-	--stats $base.stats --level 4.1 --keyint 14 --min-keyint 2 \
-	--ref 2 --mixed-refs --bframes 2 --weightb $xint_g \
-	--deblock -1:-1 --subme 5 \
-	--partitions p8x8,b8x8,i4x4,i8x8 --8x8dct \
-	--ipratio 1.1 --pbratio 1.1 \
-	--vbv-bufsize 14475 --vbv-maxrate 17500 --qcomp 0.5 \
-	--merange 12 --threads auto \
-	--output $outfile /dev/stdin 1440x1080 \
-	--mvrange 511 --aud ;;
-esac
